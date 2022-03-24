03f836c4e00f95369e1d02b1600e6ca182929804
EliteDev Emon <elitedevemon@gmail.com>
2022-03-24 10:35:21 +0600

    initial commit

---
+++ templates/default.html.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+use \App\Controller\BaseControllerClass as BaseController;
+
+/** @var array $config */
+/** @var array $user */
+/** @var array $page_content */
+
+?>
+
+<h2 class="logo">
+    <a href="<?php echo $config['base_url'] . $config['home_url']; ?>">
+        <img src="<?php echo $config['logo_image']; ?>" alt="<?php echo $config['app_title']; ?>">
+        <span class="d-inline-block ml-2"><?php echo $config['app_title']; ?></span>
+    </a>
+</h2>
+
+<hr>
+
+<?php
+if(!empty($page_content['content'])){
+    echo $page_content['content'];
+}
+?>
